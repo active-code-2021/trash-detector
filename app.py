fe15f6a74a1da21009ddd4b48d775d83a7ab6d57
tehila <t99004@gmail.com>
2021-10-16 20:26:03 +0300

    backend

--- app.py
+++ app.py
@@ -1,23 +1,42 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, jsonify
 import flask_cors
 from werkzeug.utils import secure_filename
-
-
+import json
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+class user:
+    def __init__(self, name, image):
+        self.name = name
+        self.image = image
+u1 = user("tehila","c/image.jpg")
+u2 = user("miri","c/miri.jpg")
+users = [u1,u2]
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET'])
-def index():
-    return "Hi"
+def index1():
+    return names[0]
 
+# class Message(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     text = db.Column(db.Text, nullable=False)
+
+#     def as_dict(self):
+#         return {'text': self.text}        
+def fun():
+    
+    return ([m for m in users])
+def fun2():
+    return (m for m in fun())
+@app.route('/getAll', methods=['GET'])
+def index():
+    return fun2()
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -39,3 +58,4 @@ def upload_file():
 
 if __name__ == '__main__':
     app.run()
+
--- main.py
+++ main.py
@@ -18,7 +18,13 @@ import torch
 import torchvision
 from torch.utils.data import random_split
 
-
+import os
+import torch
+import torchvision
+from torch.utils.data import random_split
+import torchvision.models as models
+import torch.nn as nn
+import torch.nn.functional as F
 data_dir  = '../try/Garbage classification/Garbage classification'
 
 classes = os.listdir(data_dir)
@@ -41,7 +47,7 @@ def show_sample(img, label):
     plt.imshow(img.permute(1, 2, 0))
 
 img, label = dataset[500]
-show_sample(img, label)
+#show_sample(img, label)
 
 random_seed = 42
 torch.manual_seed(random_seed)
@@ -65,4 +71,81 @@ def show_batch(dl):
         ax.imshow(make_grid(images, nrow = 16).permute(1, 2, 0))
         break
 
-show_batch(train_dl)
+if __name__ == '__main__':
+    show_batch(train_dl)
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch {}: train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch+1, result['train_loss'], result['val_loss'], result['val_acc']))
+    
+class ResNet(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        # Use a pretrained model
+        self.network = models.resnet50(pretrained=True)
+        # Replace last layer
+        num_ftrs = self.network.fc.in_features
+        self.network.fc = nn.Linear(num_ftrs, len(dataset.classes))
+    
+    def forward(self, xb):
+        return torch.sigmoid(self.network(xb))
+
+model = ResNet()
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+    
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    #if __name__ == '__main__':
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+            
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            if __name__ == '__main__':
+                yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+
+device = get_default_device()
+print(device)
